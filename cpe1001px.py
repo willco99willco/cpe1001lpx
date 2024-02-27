11f546f55bdfe3f0d5848722faf4237d2de9d12f
William Conner <williamconner@Williams-MacBook-Pro-2.local>
2024-02-26 22:38:08 -0500

    change

--- cpe1001px.py
+++ cpe1001px.py
@@ -1,1 +1,5 @@
-print ("Hello World")
+r="+-----------+\n| REGISTERS |\n| R0: PR0   |\n| R1: PR1   |\n| R2: PR2   |\n| R3: PR3   |\n| R4: PR4   |\n+-----------+"
+print(r.replace("PR0","9"))
+
+m="+--------+-------+\n| MEMORY |       |\n| D0: PD0  | D1: PD1 |\n| D2: PD2  | D3: PD3 |\n| D4: PD4  | D5: PD5 |\n| D6: PD6  | D7: PD7 |\n| D8: PD8  | D9: PD9 |\n+--------+-------+"
+print(m.replace("PD0","-8"))
